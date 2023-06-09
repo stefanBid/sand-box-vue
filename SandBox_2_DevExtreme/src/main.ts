f503a371b1f5617d010d1fd0527e0d9763ca7323
Stefano Biddau <biddau.stefano99@gmail.com>
2023-06-09 09:17:52 +0200

    Feat: Aggiunta SandBox 2 per DevExtreme
    Aggiunta di un nuovo progetto Sand-Box-2-DevExtreme
    dove si possono testare le componenti di DevExtreme
    in completa sicurezza.

---
+++ SandBox_2_DevExtreme/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './assets/tailwind.css'
+import 'devextreme/dist/css/dx.light.css';
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ SandBox_2_DevExtreme/src/models.ts
@@ -0,0 +1,5 @@
+export interface Project{
+    id: string;
+    businessesIds:string[];
+    status:number;
+}
